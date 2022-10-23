4746f8d822cb7e377fb1ff4e7f7251a6ec5e299c
Oscar Tenorio <otenorio90@gmail.com>
2022-10-23 18:03:12 -0400

    bus tracker progress, no marker

--- Real_Time_Bus_Tracker/mapanimation.js
+++ Real_Time_Bus_Tracker/mapanimation.js
@@ -1,100 +1,3 @@
-// var map;
-// var markers = [];
-
-// // load map
-
-// const accessToken = 'pk.eyJ1Ijoib3NjYXJtYXBib3gxMSIsImEiOiJjbDlqNTFuODQwZTdlM3Vwa2Nvd3Z6c3oyIn0.DeOlW6DMo29zR-NQVwj_vw';
- 
-// const script = document.getElementById('search-js');
-// script.onload = function() {
-// mapboxsearch.autofill({
-//     accessToken
-// });
-// };
-
-// function init(){
-// 	var myOptions = {
-// 		zoom      : 14,
-// 		center    : { lat:42.353350,lng:-71.091525},
-// 		mapTypeId : google.maps.MapTypeId.ROADMAP
-// 	};
-// 	var element = document.getElementById('map');
-//   	map = new google.maps.Map(element, myOptions);
-//   	addMarkers();
-// }
-
-// // Add bus markers to map
-// async function addMarkers(){
-// 	// get bus data
-// 	var locations = await getBusLocations();
-
-// 	// loop through data, add bus markers
-// 	locations.forEach(function(bus){
-// 		var marker = getMarker(bus.id);		
-// 		if (marker){
-// 			moveMarker(marker,bus);
-// 		}
-// 		else{
-// 			addMarker(bus);			
-// 		}
-// 	});
-
-// 	// timer
-// 	console.log(new Date());
-// 	setTimeout(addMarkers,15000);
-// }
-
-// // Request bus data from MBTA
-// async function getBusLocations(){
-// 	var url = 'https://api-v3.mbta.com/vehicles?api_key=pk.eyJ1Ijoib3NjYXJtYXBib3gxMSIsImEiOiJjbDlqNTFuODQwZTdlM3Vwa2Nvd3Z6c3oyIn0.DeOlW6DMo29zR-NQVwj_vw';	
-// 	var response = await fetch(url);
-// 	var json     = await response.json();
-// 	return json.data;
-// }
-
-// function addMarker(bus){
-// 	var icon = getIcon(bus);
-// 	var marker = new google.maps.Marker({
-// 	    position: {
-// 	    	lat: bus.attributes.latitude, 
-// 	    	lng: bus.attributes.longitude
-// 	    },
-// 	    map: map,
-// 	    icon: icon,
-// 	    id: bus.id
-// 	});
-// 	markers.push(marker);
-// }
-
-// function getIcon(bus){
-// 	// select icon based on bus direction
-// 	if (bus.attributes.direction_id === 0) {
-// 		return 'red.png';
-// 	}
-// 	return 'blue.png';	
-// }
-
-// function moveMarker(marker,bus) {
-// 	// change icon if bus has changed direction
-// 	var icon = getIcon(bus);
-// 	marker.setIcon(icon);
-
-// 	// move icon to new lat/lon
-//     marker.setPosition( {
-//     	lat: bus.attributes.latitude, 
-//     	lng: bus.attributes.longitude
-// 	});
-// }
-
-// function getMarker(id){
-// 	var marker = markers.find(function(item){
-// 		return item.id === id;
-// 	});
-// 	return marker;
-// }
-
-// window.onload = init;
-
 mapboxgl.accessToken = 'pk.eyJ1Ijoib3NjYXJtYXBib3gxMSIsImEiOiJjbDlqNTFuODQwZTdlM3Vwa2Nvd3Z6c3oyIn0.DeOlW6DMo29zR-NQVwj_vw';
 
 const map = new mapboxgl.Map({
@@ -104,6 +7,64 @@ const map = new mapboxgl.Map({
     zoom: 12
 });
 
-var marker = new mapboxgl.Marker ()
-    .setLngLat([-71.091542,42.358862])
+
+
+var counter = 0
+
+function move(){
+
+    var marker = new mapboxgl.Marker ()
+    .setLngLat([-71.092761,42.357575])
     .addTo(map);
+
+    const busStops = [
+        [-71.093729,42.359244],
+        [-71.094915,42.360175],
+        [-71.095800,42.360698],
+        [-71.099558,42.362953],
+        [-71.103476,42.365248],
+        [-71.106067,42.366806],
+        [-71.108717,42.368355],
+        [-71.110799,42.369192],
+        [-71.113095,42.370218],
+        [-71.115476,42.372085],
+        [-71.117585,42.373016],
+        [-71.118625,42.374863]
+    ];
+
+    setTimeout(() => {
+        if (counter >= busStops.length) return;
+        marker.setLngLat(busStops[counter]);
+        counter++;
+        move();
+    }, 1000);
+};
+
+// hmmmmmm live data bit below
+
+async function getBusLocations(){
+    const url = 'https://api-v3.mbta.com/vehicles?filter[route]=1&include=trip';
+    const response = await fetch(url);
+    const json     = await response.json();
+    return json.data;
+}
+
+
+async function run() {
+    const locations = await getBusLocations();
+
+    console.log(Date());
+    console.log(locations)
+
+    // let marker = new mapboxgl.Marker()
+    // marker.setLngLat([locations[0].attributes.latitude, locations[0].attributes.longitude])
+    // .addTo(map);
+    
+    var marker = new mapboxgl.Marker();
+    marker.setLngLat([locations[0].attributes.latitude, locations[0].attributes.longitude]);
+    marker.addTo(map);
+
+    // timer
+    setTimeout(run, 15000)
+};
+run();
