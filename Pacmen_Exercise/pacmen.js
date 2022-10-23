2845ac76dc67ad29af574cb74278c23ed5855e2b
Oscar Tenorio <otenorio90@gmail.com>
2022-10-22 22:37:15 -0400

    pacmen progress

--- Pacmen_Exercise/pacmen.js
+++ Pacmen_Exercise/pacmen.js
@@ -20,17 +20,17 @@ function setToRandomPosition(x, y) {
 }
 
 var game = document.getElementById('game');
-// Factory to make a PacMan
-function makePac() {
-  // var offsets = document.getElementById('game').getBoundingClientRect();
-  // var gameDimensions = document.getElementById('game').getBoundingClientRect();
-  let focus = 2
-  let velocity = setToRandom(10);
-  let position = setToRandomPosition(game.offsetWidth, game.offsetHeight);
 
-  // Add image to div id = game
+function makePac() {
+  let focus = 2
+  let direction = 0;
+  let velocity = setToRandom(10);
+  let coordinates = game.getBoundingClientRect();
+  // let position = {x: coordinates.left, y: coordinates.top};
+  let position = {x: 0, y: 0};
+
   let newimg = document.createElement('img');
-  newimg.style.position = 'sticky';
+  newimg.style.position = 'relative';
   newimg.src = './images/PacMan1.png';
   newimg.width = 50;
   newimg.style.left = position.x + 'px';
@@ -40,18 +40,17 @@ function makePac() {
     position,
     velocity,
     newimg,
-    focus
+    focus,
+    direction
   };
 }
-
-var direction = 0;
 
 function update() {
   //loop over pacmen array and move each one and move image in DOM
   pacMen.forEach((item) => {
     item.focus = (item.focus + 1) % 2;
-    direction = checkPageBounds(direction, item.newimg.width, item.position.x, game.offsetWidth);
-    item.newimg.src = pacArray[direction][item.focus];
+    item.direction = checkPageBounds(item.direction, item.newimg.width, item.position.x, window.innerWidth);
+    item.newimg.src = pacArray[item.direction][item.focus];
     checkCollisions(item);
 
     item.position.x += item.velocity.x;
@@ -61,24 +60,24 @@ function update() {
     item.newimg.style.top = item.position.y + "px";
 
   });
-  setTimeout(update, 150);
+  setTimeout(update, 50);
 };
 
 function checkCollisions(item) {
   if (
-    item.position.x + item.velocity.x + item.newimg.width > game.offsetWidth ||
+    item.position.x + item.velocity.x + item.newimg.width > window.innerWidth ||
     item.position.x + item.velocity.x < 0
   )
     item.velocity.x = -item.velocity.x;
   if (
-    item.position.y + item.velocity.y + item.newimg.height > game.offsetHeight ||
-    item.position.y + item.velocity.y < 0
+    item.position.y + item.velocity.y + item.newimg.height > game.offsetHeight - item.newimg.height ||
+    item.position.y + item.velocity.y < 0 - item.newimg.height
   )
     item.velocity.y = -item.velocity.y;
 }
 
-function checkPageBounds(direction, imgWidth, pos, pageWidth) {
-  if (pos + imgWidth >= pageWidth){
+function checkPageBounds(direction, imgWidth, pos, containerWidth) {
+  if (pos + imgWidth >= containerWidth){
     direction = 1;
   } else if (pos < 0){
     direction = 0;
