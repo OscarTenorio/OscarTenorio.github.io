f529cca094269e8586c629889d4ad451c689bc3c
Oscar Tenorio <otenorio90@gmail.com>
2023-01-28 16:40:41 -0500

    finished fireEvent section

--- References/Notes/Module_20_intro_create_react_app/20.16_testing_fire_event/todo/src/App.test.js
+++ References/Notes/Module_20_intro_create_react_app/20.16_testing_fire_event/todo/src/App.test.js
@@ -1,10 +1,42 @@
-import { render } from '@testing-library/react';
+//grabbing fireEvent from react testing library as well
+import { render, fireEvent } from '@testing-library/react';
+//grabbing an easier to read method for something similar to fireEvent
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
 test('Todo', () => {
   const { getByText, getByLabelText } = render(<App/>);
 
   getByText('TODO');
-  getByLabelText('Add Todo:')
-  getByText('Add #1')
+  getByLabelText('Add Todo:');
+  getByText('Add #1');
+});
+
+//fireEvent() submits a form (it fires an event)
+test('add items to list', () => {
+  const { getByText, getByLabelText } = render(<App/>);
+
+  getByText('TODO');
+  const input = getByLabelText('Add Todo:');
+  //NOTE: syntax, adds the text then clicks the button
+  fireEvent.change(input, {target:{value:'wash car'}});
+  fireEvent.click(getByText('Add #1'));
+
+  //then you should confirm data appears on screen
+  //reminder number increments based on index + 1
+  getByText('Add #2');
+  getByText('wash car');
+});
+
+//userEvent expresses intent better
+test('user-events allows users to add...', () => {
+  const { getByText, getByLabelText } = render(<App/>);
+
+  const input = getByLabelText('Add Todo:');
+  const button = getByText('Add #1');
+
+  userEvent.type(input, 'Learn Spanish');
+  userEvent.click(button);
+  getByText('Learn Spanish');
+  getByText('Add #2');
 });
