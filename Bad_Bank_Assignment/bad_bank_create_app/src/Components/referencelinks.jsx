d23bf411a628ef2ed37bdda41953b018f7a3390b
Oscar Tenorio <otenorio90@gmail.com>
2023-02-05 01:45:09 -0500

    final project submission

--- Bad_Bank_Assignment/bad_bank_create_app/src/Components/referencelinks.jsx
+++ Bad_Bank_Assignment/bad_bank_create_app/src/Components/referencelinks.jsx
@@ -5,7 +5,7 @@ function ReferenceLinks(){
     <p className="link text-danger text-end">
       <a className="mx-3" href="https://github.com/OscarTenorio/oscartenorio.github.io/tree/main/Bad_Bank_Assignment" target="“_blank”">Source Code: oscartenorio.github.io</a>
        | 
-      <a className="mx-3" href="#" target="“_blank”">Video Walkthrough: Youtube</a>
+      <a className="mx-3" href="https://youtu.be/GEuvdyjoJo8" target="“_blank”">Video Walkthrough: Youtube</a>
     </p>
   </div>)
 }
