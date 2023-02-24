a03446b069963bf9e82d86cda00c63ac446de09a
Oscar Tenorio <otenorio90@gmail.com>
2023-02-23 22:03:00 -0500

    finalized POST notes

--- References/Notes/Module_22_three_tiered_applications/22.2_initialize_data_store/http_server.js
+++ References/Notes/Module_22_three_tiered_applications/22.2_initialize_data_store/http_server.js
@@ -12,8 +12,9 @@ const db = low(adapter);
 // init the data store
 db.defaults({ users: [] }).write();
 
-// data parser - used to parse post data
+// data parser - used to parse post data -> NOTE: needed to run this!!!
 var bodyParser = require('body-parser');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -24,7 +25,39 @@ app.get('/data', function(req,res) {
 
 // post route
 app.post('/test', function (req, res){
-  console.log('Username: ',req.body.username, 'Password: ', req.body.password);
+  console.log(
+    'Username: '  + req.body.username,
+    ', Password: '  + req.body.password,
+    ', Name: '      + req.body.name,
+    ', Birth Date: '+ req.body.dob
+  );
+  res.send(
+    'Username: '  + req.body.username,
+    'Password: '  + req.body.password,
+    'Name: '      + req.body.name,
+    'Birth Date: '+ req.body.dob
+  );
+})
+
+// add user, reminder req = request, res = response
+app.post('/add', function (req,res) {
+  var user = {
+    'name'          : req.body.name,
+    'dob'           : req.body.dob,
+    'email'         : req.body.email,
+    'username'      : req.body.username,
+    'password'      : req.body.password,
+    'phone'         : req.body.phone,
+    'streetaddress' : req.body.streetaddress,
+    'citystatezip'  : req.body.citystatezip,
+    'latitude'      : req.body.latitude,
+    'latitude'      : req.body.latitude,
+    'longitude'     : req.body.longitude,
+    'avatar'        : req.body.avatar
+  };
+  db.get('users').push(user).write();
+  console.log(db.get('users').value());
+  res.send(db.get('users').value);
 })
 
 //start server
