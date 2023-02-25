d51e1ee780c077ba4224936d4804a2db98f57280
Oscar Tenorio <otenorio90@gmail.com>
2023-02-25 16:30:24 -0500

    module 22 file additions

--- References/Notes/Module_22_three_tiered_applications/22.2_initialize_data_store/http_server.js
+++ References/Notes/Module_22_three_tiered_applications/22.2_initialize_data_store/http_server.js
@@ -8,15 +8,26 @@ const low = require('lowdb');
 const fs = require('lowdb/adapters/FileSync');
 const adapter = new fs('db.json');
 const db = low(adapter);
+const cors    = require('cors');
 
 // init the data store
 db.defaults({ users: [] }).write();
 
+// serve static files from public directory
+app.use(express.static('public'));
+
+// allow cross-origin resource sharing (CORS)
+app.use(cors());
+
 // data parser - used to parse post data -> NOTE: needed to run this!!!
 var bodyParser = require('body-parser');
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// // home page route to show user data
+// app.get('/', function(req, res) {
+//   db.get('users').value();
+// })
 
 //return all users
 app.get('/data', function(req,res) {
