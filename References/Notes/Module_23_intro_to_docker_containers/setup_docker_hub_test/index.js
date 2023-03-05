6b3e84a3179d96892fc1146c4741ae45021300a1
Oscar Tenorio <otenorio@aquent.com>
2023-03-05 13:07:39 -0500

    docker hub notes and files

---
+++ References/Notes/Module_23_intro_to_docker_containers/setup_docker_hub_test/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app     = express();
+
+app.get('/', function(req, res){
+    console.log(new Date().toLocaleString());
+    res.send(new Date().toLocaleString());
+});
+
+var port = 3000;
+app.listen(port, function(){
+    console.log('Listening on port: ' + port);
+    console.log('This is a test to verify docker hub functionality');
+})
