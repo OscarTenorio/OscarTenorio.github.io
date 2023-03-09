7fb52cfc9f5b3aebd21b0b626d919110e2ebd6f8
Oscar Tenorio <otenorio@aquent.com>
2023-03-08 19:56:06 -0500

    adds notes to securely serving static files

--- References/Module_24_express_serever_and_graphQL/express-and-graphql-self-study-activities_starter/Serving Static Files/express-routes-master/index.js
+++ References/Module_24_express_serever_and_graphQL/express-and-graphql-self-study-activities_starter/Serving Static Files/express-routes-master/index.js
@@ -4,13 +4,16 @@ var restaurants = [{id:0,name:"Woodshill"},{id:1,name:"Fiorellas"}]
 
 const app = express();
 app.use(express.json());
+// takes this as options when accessing the public directory
 let options = {
     dotfiles: "ignore",
     redirect:false
 }
 
+// give the server access to the public folder, and use the above defined "options" as a second parameter
 app.use(express.static('public',options))
 
+// you can have the server go looking for a file as done below (and even return html!)
 app.get("/",(req,res)=>{
 let html = "<img src='img/tenor.gif'/>"
 let key = "<a href='.secret/key.txt'>secret key </a>"
